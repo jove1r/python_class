0583d151ef2083a978ee91c12d54b2c6f31823a9
jove1r <alonzod@gmail.com>
2017-08-30 19:55:45 -0700

    Initial commit

---
+++ age_check.py
@@ -0,0 +1,11 @@
+# name = input("What is your name foo: ")
+# age = int(input("Please provide your age {0}?" .format(name)))
+#if age >=18 and age <31:
+#    print("Welcome to the club, {0}".format(name))
+#else:
+#    print("Feck off ya cunt")
+
+greeting = ("good ")
+greeting += ("morning")
+print (greeting)
+
---
+++ challenge.py
@@ -0,0 +1,16 @@
+ipAddress = input("Please provide an ip adress ")
+
+segment = 1
+segment_length = 0
+
+for character in ipAddress:
+    if character == '.':
+        print("segment {} contains {} characters".format(segment,segment_length))
+        segment += 1
+        segment_length = 0
+    else:
+        segment_length += 1
+
+#unless the final character ewas a '.' then we havent printed the last segment
+if character != '.':
+    print("segment {} contains {} characters".format(segment, segment_length))
