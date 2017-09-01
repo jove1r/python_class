7cc1b39889073559428864a0d132ec6d6c3dc471
jove1r <alonzod@gmail.com>
2017-08-31 18:41:17 -0700

    added on character initialization a print of no address found if no address entered

--- range.py
+++ range.py
@@ -1,2 +1,6 @@
-for i in range(10):
-    print("i is now {}" .format(i))
+# for i in range(10):
+#    print("i is now {}" .format(i))
+i = 0
+while i < 10:
+    print("i is now {}".format(i))
+i += 1
