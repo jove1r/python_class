fd90d7364bc155db179e720583901537603b4d44
jove1r <alonzod@gmail.com>
2017-08-31 18:53:22 -0700

    range project for and while loop exercises

--- range.py
+++ range.py
@@ -1,6 +1,15 @@
 # for i in range(10):
 #    print("i is now {}" .format(i))
-i = 0
-while i < 10:
-    print("i is now {}".format(i))
-i += 1
+# i = 0
+# while i < 10:
+#     print("i is now {}".format(i))
+#     i += 1
+available_exits = ("east", "north east", "south")
+
+chosen_exit = ""
+while chosen_exit not in available_exits:
+    chosen_exit = input("Please choose a direction: ")
+
+print("Aren't you glad you found the right exit?")
+
+
