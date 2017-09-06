83a05f3eaadeee7977e62a0113e3e62eda868068
jove1r <alonzod@gmail.com>
2017-09-06 15:14:17 -0700

    tuple sorting exercise

--- while_L_practice.py
+++ while_L_practice.py
@@ -5,10 +5,17 @@
 #         break
 #
 # print(mysum)
-numberOfLoops = 0
-numberOfApples = 2
-while numberOfLoops < 10:
-    numberOfApples *= 2
-    numberOfApples += numberOfLoops
-    numberOfLoops -= 1
-print("Number of apples: " + str(numberOfApples))
+# numberOfLoops = 0
+# numberOfApples = 2
+# while numberOfLoops < 10:
+#    numberOfApples *= 2
+#    numberOfApples += numberOfLoops
+#    numberOfLoops -= 1
+# print("Number of apples: " + str(numberOfApples))
+num = 0
+while num <= 5:
+    print(num)
+    num += 1
+
+print("Outside of loop")
+print(num) 
