93805201d3d94cf73283bccb64658cd6c2081e58
jove1r <alonzod@gmail.com>
2017-09-05 19:45:02 -0700

    tuple sorting exercise

---
+++ lost_maze.py
@@ -0,0 +1,4 @@
+n = input("You are lost in the forest, do you go left or right?")
+while n == "right":
+    n = input("You are lost in the forest, do you go left or right")
+print("You got out of the forest!")
---
+++ while_L_practice.py
@@ -0,0 +1,14 @@
+# mysum = 0
+# for i in range(5, 11, 2):
+#     mysum += i
+#     if mysum == 5:
+#         break
+#
+# print(mysum)
+numberOfLoops = 0
+numberOfApples = 2
+while numberOfLoops < 10:
+    numberOfApples *= 2
+    numberOfApples += numberOfLoops
+    numberOfLoops -= 1
+print("Number of apples: " + str(numberOfApples))
